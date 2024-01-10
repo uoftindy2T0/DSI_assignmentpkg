7d7db58cbb6952345ee743dcd8abdb746bbc9229
Simeon Wong <dtxe@users.noreply.github.com>
2024-01-10 00:51:00 +0000

    initial commit

---
+++ assignmentpkg/Analysis.py
@@ -0,0 +1,37 @@
+from typing import Any, Optional
+import matplotlib
+import matplotlib.pyplot as plt
+import yaml
+
+
+class Analysis2Class():
+    pass
+
+class AnalysisClass():
+    def __init__(self, analysis_config:str):
+        CONFIG_PATHS = ['configs/system_config.yml', 'configs/user_config.yml']
+
+        # add the analysis config to the list of paths to load
+        paths = CONFIG_PATHS + [analysis_config]
+
+        # initialize empty dictionary to hold the configuration
+        config = {}
+
+        # load each config file and update the config dictionary
+        for path in paths:
+            with open(path, 'r') as f:
+                this_config = yaml.safe_load(f)
+            config.update(this_config)
+
+        self.config = config
+        
+
+    def load_data(self):
+        self.config['data_path']
+        pass
+
+    def compute_analysis(self):
+        pass
+
+    def plot_data(self, save_path:Optional[str] = None) -> plt.Figure:
+        pass
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='assignmentpkg',
+    version='0.1.0',
+    author='Simeon Wong',
+    packages=['assignmentpkg'],
+    requires=['matplotlib', 'pandas']
+)
